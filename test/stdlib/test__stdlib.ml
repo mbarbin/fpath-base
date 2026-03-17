dd323bff2af94a441d30c7d8bdcc7159e29385c2
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-03-17 13:40:56 +0100

    Move stdlib tests to the expected place

--- test/stdlib/test__stdlib.ml
+++ test/stdlib/test__stdlib.ml
@@ -4,11 +4,24 @@
 (*  SPDX-License-Identifier: MIT                                                 *)
 (*********************************************************************************)
 
-(* This is to silence `dune build @unused-libs` and keeping intended deps. *)
-open! Stdlib_for_test
+let%expect_test "require" =
+  require_does_raise (fun () -> require false);
+  [%expect {| Failure("Required condition does not hold") |}];
+  ()
+;;
 
-let%expect_test "empty" =
-  ();
-  [%expect {||}];
+let%expect_test "require_does_raise did not raise" =
+  (match require_does_raise ignore with
+   | () -> assert false
+   | exception exn -> print_string (Printexc.to_string exn));
+  [%expect {| ("Did not raise.", {}) |}];
+  ()
+;;
+
+let%expect_test "require_equal not equal" =
+  (match require_equal (module Int) 0 42 with
+   | () -> assert false
+   | exception exn -> print_string (Printexc.to_string exn));
+  [%expect {| ("Values are not equal.", { v1 = 0; v2 = 42 }) |}];
   ()
 ;;
